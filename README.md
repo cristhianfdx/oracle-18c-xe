160f15b31841de57d45462de82bae3775c50746a
Cristhian Forero <cristhianforerod@gmail.com>
2020-08-22 01:50:16 -0500

    Added install.sh

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## Prerequisites
 
 [Download](https://www.oracle.com/technetwork/database/database-technologies/express-edition/downloads/index.html) the RPM from Oracle Technology Network and save to folder. We will assume it is in `~/Downloads/oracle-database-xe-18c-1.0-1.x86_64.rpm`.
+
+
+## Run container
+
+`docker-compose up -d --build`
---
+++ install.sh
@@ -0,0 +1,36 @@
+echo "-------------------------------"
+echo "Oracle 18c-XE DATABASE INSTALL"
+echo "-------------------------------"
+
+downloads_folder=~/Downloads
+installer_file="oracle-database-xe-18c-1.0-1.x86_64.rpm"
+
+if [ ! -d "~/docker-services" ]; then
+    mkdir -p ~/docker-services/oracle/18c-xe
+    mkdir -p ~/docker-services/oracle/tablespaces
+    sudo apt install wget unzip -y
+    wget https://www.dropbox.com/s/d1lcv1oaykzxxev/sqlcl.zip?dl=0 -O sqlcl.zip
+    unzip sqlcl.zip
+    mv sqlcl ~/docker-services/oracle/
+    rm -rf sqlcl.zip
+    echo "alias sqlplus='sh -c "~/docker-services/oracle/sqlcl/bin/sql"'" >> ~/.bashrc
+fi
+
+if [ ! -d $downloads_folder ]; then
+    downloads_folder =~/Descargas
+fi
+
+if [ ! -f $PWD/18c-xe/files/$installer_file ]; then
+    mv $downloads_folder/$installer_file $PWD/18c-xe/files/
+fi
+
+docker-compose up -d --build
+
+docker-compose logs -f
+
+echo "alias oraclestart='sh -c "docker start oracledb"" >> ~/.bashrc
+echo "alias oraclestop='sh -c "docker start oracledb"" >> ~/.bashrc
+
+
+
+
