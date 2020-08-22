76d7d6fc0b6403345466316b4f742fa0db36fbea
Cristhian Forero <cristhianforerod@gmail.com>
2020-08-22 12:50:06 -0500

    Updated install.sh and aliases

--- install.sh
+++ install.sh
@@ -11,7 +11,6 @@ installer_file="oracle-database-xe-18c-1.0-1.x86_64.rpm"
 
 if [ ! -d "~/docker-services" ]; then
     mkdir -p ~/docker-services/oracle/18c-xe
-    mkdir -p ~/docker-services/oracle/tablespaces
     sudo apt install wget unzip -y
     wget https://www.dropbox.com/s/d1lcv1oaykzxxev/sqlcl.zip?dl=0 -O sqlcl.zip
     unzip sqlcl.zip
